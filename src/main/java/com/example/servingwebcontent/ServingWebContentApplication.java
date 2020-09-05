4f8b9b8bbc5dcbc815a1f9f2821a32c12ca9f17e
subr1990 <subramaniank@live.com>
2020-09-05 19:25:30 +1000

    dgdsgs

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -2,10 +2,16 @@ package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServingWebContentApplication {
-
+    @RequestMapping("/api")
+    public String greeting(){
+        return "greeting";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
